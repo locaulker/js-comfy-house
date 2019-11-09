f338d7b1dec754560f4c46fbe865d89a5dd2174e
Lawrence <Caulker>
2019-11-09 18:53:56 -0500

    completed adding products dynamically

--- js/App.js
+++ js/App.js
@@ -11,14 +11,50 @@ const productsDOM = document.querySelector(".products-center");
 // The Cart
 let cart = [];
 
-// Getting Products
+// Get Products
 class Products {
+  async getProducts() {
+    try {
+      let result = await fetch('../data/products.json');
+      let data = await result.json();
 
+      let products = data.items;
+      products = products.map(item => {
+        const {title, price} = item.fields;
+        const {id} = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return {title, price, id, image};
+      })
+      return products;
+
+    } catch (error) {
+      console.log(error);
+    }  
+  }
 }
 
 // Display Products
 class UI {
-
+  displayProducts(products) {
+    // console.log(products);
+    let result = '';
+    products.forEach(product => {
+      result += `
+      <article class="product">
+        <div class="img-container">
+          <img src="${product.image}" alt="product 01" class="product-img">
+          <button class="bag-btn" data-id="${product.id}">
+            <i class="fad fa-shopping-cart"></i>
+            add to cart
+          </button>
+        </div>
+        <h3>${product.title}</h3>
+        <h4>$${product.price}</h4>
+      </article>
+      `
+    });
+    productsDOM.innerHTML = result;
+  }
 }
 
 // Local Storage
@@ -31,5 +67,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const ui = new UI();
   const products = new Products();
+
+  // get all products
+  products.getProducts().then(products => ui.displayProducts(products));
 
 });
