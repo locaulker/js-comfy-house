395919f4e8784033ca0510bdcb3d7e6362bd64f0
Lawrence <Caulker>
2019-11-09 13:37:16 -0500

    setup JS structure

---
+++ js/App.js
@@ -0,0 +1,35 @@
+// Declare Cart Variables
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+
+// The Cart
+let cart = [];
+
+// Getting Products
+class Products {
+
+}
+
+// Display Products
+class UI {
+
+}
+
+// Local Storage
+class Storage {
+
+}
+
+// Event Listener
+document.addEventListener("DOMContentLoaded", () => {
+
+  const ui = new UI();
+  const products = new Products();
+
+});
